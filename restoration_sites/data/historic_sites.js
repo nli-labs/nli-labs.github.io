b5383ffca2fef6d30d24e5f1645804ea1a7f2b43
idoivri <idoivri@gmail.com>
2014-10-07 04:54:43 +0300

    add geojson

---
+++ restoration_sites/data/historic_sites.js
@@ -0,0 +1,150 @@
+var stations = {
+
+
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+         35.003197,
+         32.596897
+        ]
+      },
+      "properties": {
+        "sitename": "בית הכנסת ובית השוחט בבת שלמה",
+        "gush": "11345",
+        "helka": "16",
+        "address": "רחוב המושבה",
+        "contruction_year": "1888-1889",
+        "preservation_file_date" : "אוגוסט 2013",
+        "file_editor_1" : "אדר' טל פרידמן ראודניץ",
+        "file_editor_2" : "אדר' לילך גינת נוימרק",
+        "file_editor_3" : "",
+        "link_to_file" : "https://www.dropbox.com/s/ppys5ly5hjeklbg/%2B%C2%AC%2B%C3%96%2B%C2%BA_%2B%C2%AC%2B%C3%96%2B%C3%B3%2B%C3%B2%2B%C3%B4_%2B%C3%A6%2B%C3%96%2B%C2%AC_%2B%C3%B6%2B%C2%A2%2B%C3%A1%2B%C3%AD%2B%C2%AC_%2B%C3%A6%2B%C3%A6%2B%C2%AC_%2B%C2%AC%2B%C2%A3%2BP%2B%C3%B6.pdf?dl=0",
+        "thumbnail" : "img/bat-shlomo.png"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [   
+        35.224177,
+        31.771555
+        ]
+      },
+      "properties": {
+        "sitename": "טחנת הרוח במשכנות שאננים",
+        "gush": "30029",
+        "helka": "12",
+        "address": "רחוב היינה",
+        "contruction_year": "1857",
+        "preservation_file_date" : "אוגוסט 2011",
+        "file_editor_1" : "ג. קרטס",
+        "file_editor_2" : "",
+        "file_editor_3" : "",
+        "link_to_file" : "https://www.dropbox.com/s/upr9nov3nv7iqrg/%2B%C2%AC%2B%C3%B9%2B%C3%A1%2B%C2%AC_%2B%C3%B6%2B%C2%BA%2BP%2B%C3%B9_%2B%C3%A6%2BP%2B%C2%AC%2B%C2%A2%2B%C3%A1%2B%C3%B2%2B%C2%AC_%2B%C2%AC%2B%C3%89%2B%C3%A1%2B%C3%A1%2B%C3%96%2B%C2%A5_-_%2B%C3%86%2B%C3%A6%2B%C2%BF%2B%C3%96%2B%C3%89%2B%C2%A3_%2B%C2%BA%2B%C2%BF%2B%C3%BF%2B%C3%AD.pdf?dl=0",
+        "thumbnail" : "data/windmill.png"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [   
+        34.945364,
+        32.519098
+        ]
+      },
+      "properties": {
+        "sitename": "בתי גבעת הפועל",
+        "gush": "10207",
+        "helka": "7",
+        "address": "רחוב השניים 11",
+        "construction_date": "1929",
+        "preservation_file_date" : "ספטמבר 2012",
+        "file_editor_1" : "אדר' טל פרידמן ראודניץ",
+        "file_editor_2" : "אדר' לילך גינת נוימרק",
+        "file_editor_3" : "",
+        "link_to_file" : "https://www.dropbox.com/s/u3nkip6ed6pfx3o/%2B%C3%86%2B%C3%A6%2B%C3%B3%2B%C2%AC_%2B%C3%B6%2B%C3%B1%2B%C3%B2%2B%C3%B3%2B%C2%A3_%2B%C3%A6%2B%C3%A1%2B%C3%96%2BP%2B%C3%96%2B%C3%A1%2B%C3%B6_-_%2BP%2B%C3%B3%2B%C2%AC%2B%C3%B6_%2B%C2%AC%2B%C3%96%2BP%2B%C3%B2%2B%C2%BF.pdf?dl=0",
+        "thumbnail" : ""
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [   
+        34.778197,
+        31.8138
+        ]
+      },
+      "properties": {
+        "sitename": "בתי פוקס",
+        "gush": "3878",
+        "helka": "4",
+        "address": "רחוב הבילויים 31",
+        "contruction_year": "1887-1890",
+        "preservation_file_date" : "יולי 2007",
+        "file_editor_1" : "אדר' גבריאל קרטס",
+        "file_editor_2" : "אינג' יעקב שפר",
+        "file_editor_3" : "",
+        "link_to_file" : "https://www.dropbox.com/s/hc9h4mpmbinyc8b/%2B%C3%A6%2B%C3%96%2B%C2%AC_%2B%C3%B1%2B%C3%B2%2B%C2%BA%2B%C3%AD__-_%2B%C3%86%2B%C3%A6%2B%C2%BF%2B%C3%96%2B%C3%89%2B%C2%A3_%2B%C2%BA%2B%C2%BF%2B%C3%BF%2B%C3%AD.pdf?dl=0",
+        "thumbnail" : ""
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [   
+        35.093497,
+        32.7432
+        ]
+      },
+      "properties": {
+        "sitename": "בתי הקירור",
+        "gush": "11139",
+        "helka": "9",
+        "address": "רחוב הבילויים 31",
+        "contruction_year": "1942",
+        "preservation_file_date" : "מאי 2013",
+        "file_editor_1" : "אדר' לילך גינת נוימרק",
+        "file_editor_2" : "אדר' טל פרידמן ראודניץ",
+        "file_editor_3" : "",
+        "link_to_file" : "https://www.dropbox.com/s/dif4gkled4acqmq/%2B%C3%A6%2B%C3%96%2B%C2%AC_%2B%C2%BA%2B%C3%96%2B%C2%BF%2B%C3%B2%2B%C2%BF_-__%2B%C2%A2%2B%C3%B1%2B%C2%BF_%2B%C3%B9%2B%C3%AD%2B%C3%96%2B%C3%B4%2B%C3%96%2B%C2%A5_-_%2BP%2B%C3%B3%2B%C2%AC%2B%C3%B6_%2B%C2%AC%2B%C3%96%2BP%2B%C3%B2%2B%C2%BF.pdf?dl=0",
+        "thumbnail" : ""
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [   
+        35.07,
+        32.823997
+        ]
+      },
+      "properties": {
+        "sitename": "מתחם מגדלאור",
+        "gush": "11139",
+        "helka": "9",
+        "address": "רחוב המסגר",
+        "contruction_year": "1956",
+        "preservation_file_date" : "פברואר 2013",
+        "file_editor_1" : "יעל פורמן נעמן",
+        "file_editor_2" : "אדר' אמיר פרוינדליך",
+        "file_editor_3" : "",
+        "link_to_file" : "https://www.dropbox.com/s/nexht9mc69tfe0v/MIGDAL-OR_28-03-13_fnl%20%281%29.pdf?dl=0",
+        "thumbnail" : ""
+      }
+    }
+  ]
+}
+
+
+
+
+
